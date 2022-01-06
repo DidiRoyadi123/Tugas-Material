fe805f24f899b79468ec565d7fa606c549597def
didiroyadi123 <didiroyadi123@gmail.com>
2022-01-06 13:19:52 +0700

    rev

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,35 +4,34 @@ use App\Models\User;
 
 @extends('layouts.app3')
 @push('css')
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/vendors.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/forms/wizard/bs-stepper.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/forms/select/select2.min.css') }}">
-    <!-- END: Vendor CSS-->
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/vendors.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/forms/wizard/bs-stepper.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/vendors/css/forms/select/select2.min.css') }}">
+<!-- END: Vendor CSS-->
 
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap-extended.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/colors.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/components.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/dark-layout.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/bordered-layout.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/semi-dark-layout.min.css') }}">
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap-extended.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/colors.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/components.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/dark-layout.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/bordered-layout.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/semi-dark-layout.min.css') }}">
 
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/core/menu/menu-types/vertical-menu.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/plugins/forms/form-validation.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/plugins/forms/form-wizard.min.css') }}">
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/core/menu/menu-types/vertical-menu.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/plugins/forms/form-validation.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/plugins/forms/form-wizard.min.css') }}">
 @endpush
 @section('content')
 <div class="app-content content ">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper">
         <div class="row">
             <div class="col">
-                <img src="{{ asset('/images/welcome.png') }}" class="mr-1" alt="Welcome Image" height="600"
-                width="800">
+                <img src="{{ asset('/images/welcome.png') }}" class="mr-1" alt="Welcome Image" height="600" width="800">
             </div>
             <div class="col">
                 <div class="content-body">
@@ -56,13 +55,13 @@ use App\Models\User;
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @foreach ($users as $user)
-                                <tr>
-                                    <td>{{ $user['id'] }}</td>
-                                    <td>{{ $user['name'] }}</td>
-                                    <td>{{ $user['email'] }}</td>
-                                </tr>
-                                @endforeach
+                                    <php foreach ($users as $user):?>
+                                        <tr>
+                                            <td>{{ $user['id'] }}</td>
+                                            <td>{{ $user['name'] }}</td>
+                                            <td>{{ $user['email'] }}</td>
+                                        </tr>
+                                    </php endforeach;?>
                                 </tbody>
                             </table>
                         </div>
@@ -72,31 +71,31 @@ use App\Models\User;
                 </div>
             </div>
         </div>
-        </div>
     </div>
+</div>
 @endsection
 
 
 @push('script')
 
 
-    <!-- BEGIN: Vendor JS-->
-    <script src="{{ asset('/vendors/js/vendors.min.js') }}"></script>
-    <!-- BEGIN Vendor JS-->
+<!-- BEGIN: Vendor JS-->
+<script src="{{ asset('/vendors/js/vendors.min.js') }}"></script>
+<!-- BEGIN Vendor JS-->
 
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="{{ asset('/vendors/js/forms/wizard/bs-stepper.min.js') }}"></script>
-    <script src="{{ asset('/vendors/js/forms/select/select2.full.min.js') }}"></script>
-    <script src="{{ asset('/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
-    <!-- END: Page Vendor JS-->
+<!-- BEGIN: Page Vendor JS-->
+<script src="{{ asset('/vendors/js/forms/wizard/bs-stepper.min.js') }}"></script>
+<script src="{{ asset('/vendors/js/forms/select/select2.full.min.js') }}"></script>
+<script src="{{ asset('/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
+<!-- END: Page Vendor JS-->
 
-    <!-- BEGIN: Theme JS-->
-    <script src="{{ asset('/js/core/app-menu.min.js') }}"></script>
-    <script src="{{ asset('/js/core/app.min.js') }}"></script>
-    <script src="{{ asset('/js/scripts/customizer.min.js') }}"></script>
-    <!-- END: Theme JS-->
+<!-- BEGIN: Theme JS-->
+<script src="{{ asset('/js/core/app-menu.min.js') }}"></script>
+<script src="{{ asset('/js/core/app.min.js') }}"></script>
+<script src="{{ asset('/js/scripts/customizer.min.js') }}"></script>
+<!-- END: Theme JS-->
 
-    <!-- BEGIN: Page JS-->
-    <script src="{{ asset('/js/scripts/forms/form-wizard.min.js') }}"></script>
+<!-- BEGIN: Page JS-->
+<script src="{{ asset('/js/scripts/forms/form-wizard.min.js') }}"></script>
 
 @endpush
